0aac0b668a2f722f4251cb10ec625134857db574
Thomas Mbise <tmbise@bloomberg.net>
2023-10-23 21:17:23 -0400

    Change greeting

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Greetings from Cloud commanders!"}
+    return {"message": "Greetings from Cloud commanders! This is culture forum"}
